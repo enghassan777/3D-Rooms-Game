df36d0158b75e1a2ad58e7ebac460524f81c403c
enghassan777 <dyahsn752@gmail.com>
2025-02-08 21:21:03 +0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-let currentRoom = 1;
-function openRoom(roomNumber) {
-    document.querySelectorAll('.room').forEach(room => room.classList.remove('active'));
-    document.getElementById(`room${roomNumber}`).classList.add('active');
-    if (roomNumber < 10) {
-        document.getElementById(`door${roomNumber + 1}`).style.display = 'block';
-    }
-}
